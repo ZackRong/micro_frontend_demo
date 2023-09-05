75506bd8629103be12b869f7cd1d75886f10a56a
julianrong <julianrong@futunn.com>
2023-09-05 20:42:08 +0800

    初步配置

--- base/src/index.js
+++ base/src/index.js
@@ -1,1 +1,20 @@
-document.getElementById('base').innerHTML = 'base';
+import * as singleSpa from 'single-spa';
+// import System from 'systemjs';
+
+const name1 = 'app1';
+
+// const app1 = () => import('../../app1/static/js/main.bundle.js');
+// const app1 = () => System.import('http://127.0.0.1:3000/js/main.bundle.js');
+const app1 = () => import('http://127.0.0.1:3000/js/main.bundle.js');
+
+const app1ActiveWhen = '/';
+
+singleSpa.registerApplication({
+  name: name1,
+  app: app1,
+  activeWhen: app1ActiveWhen
+});
+
+singleSpa.start();
+
+// document.getElementById('base').innerHTML = 'base';
